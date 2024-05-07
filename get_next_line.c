bbc73228e0e10c9f4968ae205f25ca6a38c3d0c0
Julia Wolfram <julia.wolfram@gmx.at>
2024-05-07 13:19:49 +0200

    refactor: did a lot of stuff honestly

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,91 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:21:31 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/04/18 19:55:18 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/05/07 13:19:19 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	amount;
+	size_t	i;
+
+	if (!nmemb || !size)
+		return (malloc(0));
+	amount = nmemb * size;
+	if (nmemb != amount / size)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < amount)
+	{
+		((char *)ptr)[i] = '\0';
+		i++;
+	}
+	return (ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] != (unsigned char)c)
+		i++;
+	if (!s[i] && (unsigned char)c)
+		return (NULL);
+	return (&((char *)s)[i]);
+}
+
 char	*get_next_line(int fd)
 {
-	
+	static char	*buffer;
+	char		*input;
+	int			bytes_read;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	input = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!input)
+		return (NULL);
+	bytes_read = 1;
+	while (!(ft_strchr(input, '\n')) && bytes_read)
+	{
+		bytes_read = read(fd, input, BUFFER_SIZE);
+		if (bytes_read == -1 || bytes_read <= 0)
+				return (free(input), NULL);
+	}
+	return (input);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+	int		count;
+
+	count = 0;
+	fd = open("overview.txt", O_RDONLY);
+	if (fd == -1)
+		return (1);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		count++;
+		printf("[%d]:%s\n", count, line);
+		free(line);
+		line = NULL;
+	}
+	close(fd);
+	return (0);	
 }
