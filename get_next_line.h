59fee96087cf24b87494433fa334ac8ca33a4a4c
Julia Wolfram <julia.wolfram@gmx.at>
2024-05-07 13:20:18 +0200

    add: BUFFER_SIZE

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,17 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:22:40 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/04/17 15:26:52 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/05/07 12:14:42 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 2
+# endif
+
 # include <stdlib.h>
 # include <unistd.h>
 
