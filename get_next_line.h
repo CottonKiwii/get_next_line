90552edbf3d2e47bc098512bb56e362d7e173277
Julia Wolfram <julia.wolfram@gmx.at>
2024-05-08 14:34:13 +0200

    chore: added functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:22:40 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/05/07 12:14:42 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/05/08 13:25:36 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,22 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 2
+# define BUFFER_SIZE 24
 # endif
 
 # include <stdlib.h>
 # include <unistd.h>
+
+/* get_next_line */
+
+void	free_fb(char **buffer);
+char	*get_next_line(int fd);
+
+/* get_next_line_utils */
+
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
 
 #endif
