f18506bded3ca138e8ceb9fed7e5d4b66d175f99
CottonKiwii <julia.wolfram@gmx.at>
2024-05-15 16:46:18 +0200

    chore: fixed norm

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: CottonKiwii <julia.wolfram@gmx.at>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:22:40 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/05/14 14:11:15 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/05/15 15:24:53 by CottonKiwii      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,19 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 24
+#  define BUFFER_SIZE 24
 # endif
 
 # include <stdlib.h>
 # include <unistd.h>
 
 /* get_next_line */
-
 void	free_fb(char **buffer);
 char	*get_next_line(int fd);
 char	*ft_get_line(char *fbuff);
 char	*ft_next_line(char *buffer, char *fbuff);
 
 /* get_next_line_utils */
-
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,34 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: CottonKiwii <julia.wolfram@gmx.at>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:22:40 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/05/14 14:11:15 by jwolfram         ###   ########.fr       */
+/*   Updated: 2024/05/15 14:34:23 by CottonKiwii      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 24
+#  define BUFFER_SIZE 24
 # endif
 
 # include <stdlib.h>
 # include <unistd.h>
 
-/* get_next_line */
-
+/* get_next_line_bonus */
 void	free_fb(char **buffer);
 char	*get_next_line(int fd);
 char	*ft_get_line(char *fbuff);
 char	*ft_next_line(char *buffer, char *fbuff);
 
-/* get_next_line_utils */
-
+/* get_next_line_utils_bonus */
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
